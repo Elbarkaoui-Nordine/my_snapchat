42c4cfb9b3f8bacc54b1ad987fe412f179d7664d
Faudelhadroug <faudel.hadroug@epitech.eu>
2020-05-28 21:29:17 +0200

    fin du projet niveaux fonctionnalité

--- my_snapchat/src/components/snap/Snap.js
+++ my_snapchat/src/components/snap/Snap.js
@@ -2,10 +2,11 @@ import React,{useState, useEffect} from 'react';
 import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
 import axios from 'axios';
 import store from '../../store';
-
+import './Snap.css';
 const Snap = () => {
 
     const [getSnaps, setGetSnap] = useState([]);
+    const [seconds, setSeconds] = useState(0);
     const token = store.getState().auth['verification']['user']['token'];
 
     let firstEntry = true
@@ -14,6 +15,26 @@ const Snap = () => {
           "token": token
         }
     }
+    let configSeen = {
+      headers: {
+        "Content-Type": "application/json",
+        "token": token
+      }
+  }
+
+    useEffect(() => {
+      let interval = null;
+      if(seconds > 0)
+      {
+        interval = setInterval(() => {
+          setSeconds(seconds => seconds - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+      }
+
+    }, [seconds]);
+
+
     useEffect(() => {
         axios
         .get('http://snapi.epitech.eu/snaps', config)
@@ -24,14 +45,15 @@ const Snap = () => {
         .catch((error) => {
           console.log(error)
       })
-        
-    }, [firstEntry])
+       console.log('gererteeererre') 
+    }, [getSnaps])
       
     const openSnap = (idSnap, durationSnap) => {
         var url = "http://snapi.epitech.eu/snap/" + idSnap
         axios
         .get(url, config)
         .then(response => {
+          setSeconds(durationSnap);
           fetch(url, {
             method: 'GET',
             headers: {
@@ -41,11 +63,20 @@ const Snap = () => {
                })
         .then((response) => response.blob())
         .then((blob) => {
-  
+
           const url = window.URL.createObjectURL(new Blob([blob]));
-          const link = document.createElement('img');
-          link.src = url;
-          document.body.appendChild(link);
+          document.getElementById('img').src = url;
+          axios
+          .post('http://snapi.epitech.eu/seen', {
+            "id": idSnap
+          }, configSeen)
+          .then(response => {
+            console.log('seen log ', response);
+          })
+          .catch((error) => {
+            console.log('seen error ', error);
+          })
+         
         })
       })
         .catch((error) => {
@@ -58,7 +89,10 @@ const Snap = () => {
             <Container>
             <p> All snap</p>
             {getSnaps.map((snap, i) => <div onClick={() => openSnap(snap.snap_id, snap.duration)} key={snap.snap_id+snap.from+i}> {snap.from}</div>)}
-            <img src='' id='img' />
+            <div>
+              { seconds > 0 ?  <img src='' id='img' /> : null}
+              { seconds > 0 ?  <p className='seconds'> {seconds}</p> : null}
+            </div>
             <div id='box'></div>
             </Container>
         </div>
