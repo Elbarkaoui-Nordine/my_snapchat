ad5b0ff14b7242ec46a6246aed70097eec5efe64
Faudelhadroug <faudel.hadroug@epitech.eu>
2020-05-28 14:44:14 +0200

    try to get a snap

--- my_snapchat/src/components/snap/Snap.js
+++ my_snapchat/src/components/snap/Snap.js
@@ -8,27 +8,44 @@ const Snap = () => {
     const [getSnaps, setGetSnap] = useState([]);
     const token = store.getState().auth['verification']['user']['token'];
 
+    let firstEntry = true
     let config = {
         headers: {
           "token": token
         }
-      }
-      axios
-      .get('http://snapi.epitech.eu/snaps', config)
-      .then(response => {
-        if(response.data.data !== getSnaps)
-            setGetSnap(response.data.data);
+    }
+    useEffect(() => {
+        axios
+        .get('http://snapi.epitech.eu/snaps', config)
+        .then(response => {
+          if(response.data.data !== getSnaps)
+              setGetSnap(response.data.data);
       })
-      .catch((error) => {
-        console.log(error.response);
+        .catch((error) => {
+          console.log(error)
       })
-
+        
+    }, [firstEntry])
+      
+    const openSnap = (idSnap, durationSnap) => {
+        console.log(idSnap, durationSnap);
+        var url = "http://snapi.epitech.eu/snap/" + idSnap
+        console.log(url)
+        axios
+        .get(url, config)
+        .then(response => {
+            console.log(response);
+      })
+        .catch((error) => {
+          console.log(error.response);
+      })
+    }
     return(
 
         <div>
             <Container>
             <p> All snap</p>
-            {getSnaps.map((snap, i) => <div key={snap.id+snap.from+i}> {snap.from}</div>)}
+            {getSnaps.map((snap, i) => <div onClick={() => openSnap(snap.snap_id, snap.duration)} key={snap.snap_id+snap.from+i}> {snap.from}</div>)}
             </Container>
         </div>
     )
