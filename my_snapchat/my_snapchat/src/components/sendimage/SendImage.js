966abb22bf39d50b93821c620f83ac36a7b1143a
Nordine <nordine.el-barkaoui@epitech.eu>
2020-05-28 16:12:33 +0200

    a ton tour faudel

--- my_snapchat/my_snapchat/src/components/sendimage/SendImage.js
+++ my_snapchat/my_snapchat/src/components/sendimage/SendImage.js
@@ -3,28 +3,21 @@ import ImageUploader from "react-images-upload";
 import axios from 'axios';
 import store from '../../store';
 import './SendImage.css';
-const SendImage = props => {
+const App = props => {
   const [picture, setPicture] = useState([]);
   const [users, setUsers] = useState([]);
   const [selectedUsers, setSelectedUsers] = useState('');
   const [secondes, setSecondes] = useState(5);
   const token = store.getState().auth['verification']['user']['token'];
+  const onDrop = image => {
 
-  const onFileChange = (e) => {
-    let files = e.target.files || e.dataTransfer.files;
-    console.log(files[0].split('.').pop());
-    setPicture(files);
-    if (!files.length) {
-        console.log('no files');
-    }
-    console.log(files);
-    console.log(files[0])
-}
-
+    image = image[image.length-1];
+    setPicture(image)
+   
+  };
   useEffect(() => {
     getUsers();
   }, [picture])
-
   const getUsers = () => {
     if(picture.length !== 0)
     {
@@ -45,40 +38,62 @@ const SendImage = props => {
     }
   }
 
+
+
+  const onFileChange = (e) => {
+    let files = e.target.files || e.dataTransfer.files;
+    setPicture(files);
+    if (!files.length) {
+        console.log('no files');
+    }
+    console.log(files);
+    console.log(files[0])
+}
+
+
   const sendSnap = () => {
+    if(picture.length === 0) return alert('You need to pick a photo');
+    if(selectedUsers === '') return alert('You need to select a user to send your snap');
+    if(secondes > 10 || secondes < 0 || secondes === '' ) return alert ('A snap need to be between 1-10s !')
 
-
-    const bodyFormData = new FormData();
-    bodyFormData.append('duration',secondes)
-    bodyFormData.append('to',selectedUsers);
-    bodyFormData.append('image',picture[0])
-   
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data',
-            'token':token
-        }
+    let config = {
+      headers: {
+        "token": token
+      }
     }
 
     axios
-    .post('http://snapi.epitech.eu/snap',bodyFormData, config)
+    .post('http://snapi.epitech.eu/snap', {
+      duration: secondes,
+      to: selectedUsers,
+      image: picture[0]
+    }, config)
     .then(response => {
       console.log(response);
     })
     .catch((error) => {
       console.log(error.response);
     })
-  }
 
+  }
   const handleOptionChange = (changeEvent) => {
     setSelectedUsers(changeEvent.target.value);
   }
-
-
+  //onClick={}
   return (
     <div>
       <h2 className='mb-3'>Welcome to our Snap</h2>
- 
+    <ImageUploader
+      {...props}
+      withIcon={true}
+      onChange={onDrop}
+      imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+      maxFileSize={5242880}
+    />
+
+
+<input id="my-file-selector" type="file" name="file" onChange={onFileChange}></input>
+
   <div style={{height: '300px', overflow: 'scroll'}}>
     {users.length === 0 ? <h3 className='mt-5'> Choose a picture to snap </h3> : users.map((user, i) => 
     <div key={i + user.email}> 
@@ -91,9 +106,6 @@ const SendImage = props => {
       </div> 
     )}
   </div>
-
-  <input id="my-file-selector" type="file" name="file" onChange={onFileChange}></input>
-
   <div> 
     <label htmlFor="secondes">Number of secondes (1-10):</label>
     <input className='text-center ml-2' type="number" id="secondes" name="secondes"
@@ -106,4 +118,4 @@ const SendImage = props => {
   );
 };
 
-export default SendImage;
+export default App;
